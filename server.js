ed1d3e8db79341a488f0f15318250b2961386589
Yury Tsarev <yury.tsarev@gooddata.com>
2014-09-05 15:37:14 +0200

    FEATURE: PCI-4011 Introduce formatted results cache for performance increase

--- server.js
+++ server.js
@@ -355,33 +355,44 @@ var server = http.createServer(function(req,res){
   res.writeHead(200, { 'Content-Type': 'application/json' });
   var incoming = url.parse(req.url, true);
   if(/.+\.json$/.test(incoming.pathname)) {
-    fs.readFile(pathToReports + incoming.pathname, function(err,data){
-      if (err) throw err;
-      data = JSON.parse(data);
-      data.tests = formatResults(data);
-      fs.readFile(__dirname + '/report.handlebars', function(err,html){
+    function recurse() {
+      fs.readFile(pathToReports + incoming.pathname + '.parsed', function(err,data){
+        if (err) {console.log(err);
+          var data = fs.readFileSync(pathToReports + incoming.pathname);
+          data = JSON.parse(data);
+          data.tests = formatResults(data);
+          fs.writeFileSync(pathToReports + incoming.pathname + '.parsed',
+            JSON.stringify(data));
+          console.log('Reformatting json...');
+          recurse();
+        } else {
+          data = JSON.parse(data);
+          fs.readFile(__dirname + '/report.handlebars', function(err,html){
+            var template = handlebars.compile(html.toString());
+            var rendered = template(data);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(rendered);
+          });
+        }
+      });
+    };
+    recurse();
+  } else if(incoming.pathname == '/'){
+    fs.readFile(__dirname + '/index.handlebars', function(err,html){
+      fs.readdir(pathToReports, function(err, files){
+        var data = {};
+        data.files = [];
+        files.forEach(function(name){
+          if(path.extname(name) == '.json'){
+            data.files.push(name);
+          }
+        });
+
         var template = handlebars.compile(html.toString());
         var rendered = template(data);
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(rendered);
       });
-    });
-  } else if(incoming.pathname == '/'){
-      fs.readFile(__dirname + '/index.handlebars', function(err,html){
-        fs.readdir(pathToReports, function(err, files){
-          var data = {};
-          data.files = [];
-          files.forEach(function(name){
-            if(path.extname(name) == '.json'){
-              data.files.push(name);
-            }
-          }); 
-
-          var template = handlebars.compile(html.toString());
-          var rendered = template(data);
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(rendered);
-        });
     });
   } else if(/(\.js$|\.css$|\.html$)/.test(incoming.pathname)){
     serveStatic(publicHTML + incoming.pathname, res);
