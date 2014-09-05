e80d251697617d66d55cad9c4bfabbd56dc8b8bc
Yury Tsarev <yury.tsarev@gooddata.com>
2014-09-05 14:31:42 +0200

    FEATURE: PCI-4011 test report listing

--- server.js
+++ server.js
@@ -354,16 +354,7 @@ var formatResults = function(input) {
 var server = http.createServer(function(req,res){
   res.writeHead(200, { 'Content-Type': 'application/json' });
   var incoming = url.parse(req.url, true);
-  if(incoming.pathname == '/reports'){
-    fs.readdir(pathToReports, function(err, files){
-      var filteredFiles = [];
-      files.forEach(function(name){
-        if(path.extname(name) == '.json'){
-          filteredFiles.push(name);
-        }
-      }); 
-      res.end(JSON.stringify(filteredFiles))});
-  } else if(/.+\.json$/.test(incoming.pathname)) {
+  if(/.+\.json$/.test(incoming.pathname)) {
     fs.readFile(pathToReports + incoming.pathname, function(err,data){
       if (err) throw err;
       data = JSON.parse(data);
@@ -374,10 +365,24 @@ var server = http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(rendered);
       });
-      //       res.end(JSON.stringify(data));       
     });
   } else if(incoming.pathname == '/'){
-    serveStatic(publicHTML + '/index.html', res);
+      fs.readFile(__dirname + '/index.handlebars', function(err,html){
+        fs.readdir(pathToReports, function(err, files){
+          var data = {};
+          data.files = [];
+          files.forEach(function(name){
+            if(path.extname(name) == '.json'){
+              data.files.push(name);
+            }
+          }); 
+
+          var template = handlebars.compile(html.toString());
+          var rendered = template(data);
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(rendered);
+        });
+    });
   } else if(/(\.js$|\.css$|\.html$)/.test(incoming.pathname)){
     serveStatic(publicHTML + incoming.pathname, res);
   } else {
